6ad6d4f4e108c2b3b8dfe9a2a74eae88ef9f1c38
EduMLAlmeida <edualmeidawebdev@gmail.com>
2022-01-26 15:33:54 -0300

    Atende ao requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  const first = data.employees.find((currentValue) => currentValue.firstName === employeeName);
+  const last = data.employees.find((currentValue) => currentValue.lastName === employeeName);
+  if (first === undefined) {
+    return last;
+  }
+  return first;
 }
 
 module.exports = getEmployeeByName;
