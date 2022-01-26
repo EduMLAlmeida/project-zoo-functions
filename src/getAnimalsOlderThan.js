073334a5aba1f541996f07b7ce6bf0c01f11124f
EduMLAlmeida <edualmeidawebdev@gmail.com>
2022-01-26 12:07:13 -0300

    Atende ao requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,15 @@
 const data = require('../data/zoo_data');
 
+const ageVerifier = (agesArray, minimumAge) => agesArray.every(
+  (currentValue) => currentValue.age >= minimumAge,
+);
+
+const findSpecie = (speciesArray, speciesName) => speciesArray.find(
+  (currentValue) => currentValue.name === speciesName,
+);
+
 function getAnimalsOlderThan(animal, age) {
-  // seu código aqui
+  return ageVerifier(findSpecie(data.species, animal).residents, age);
 }
 
 module.exports = getAnimalsOlderThan;
