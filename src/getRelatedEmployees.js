bfb63e67c35dbbd23d0ddd06d2d36c7144deaef8
EduMLAlmeida <edualmeidawebdev@gmail.com>
2022-01-26 17:38:01 -0300

    Atende ao requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,30 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((currentValue) => currentValue.managers.includes(id));
 }
 
+const test = (subordinatesArray) => {
+  if (subordinatesArray.length === 0) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+};
+
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const manager = data.employees.filter(
+    (currentValue) => currentValue.managers.includes(managerId),
+  );
+  const subordinates = manager.map(
+    (currentValue) => `${currentValue.firstName} ${currentValue.lastName}`,
+  );
+  try {
+    test(subordinates);
+    return subordinates;
+  } catch (error) {
+    throw error.message;
+  }
 }
+
+console.log(getRelatedEmployees('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
 
 module.exports = { isManager, getRelatedEmployees };
