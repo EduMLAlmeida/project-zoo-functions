10562518faaec1b3e3ac540caf0f09ae9e4db872
EduMLAlmeida <edualmeidawebdev@gmail.com>
2022-01-27 08:06:11 -0300

    Atende ao requisito 4(correção)

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,12 @@
 const data = require('../data/zoo_data');
 
 function countAnimals(animal) {
-  // seu código aqui
+  if (animal === undefined) {
+    data.species.map(() => {});
+    return 'teste';
+  }
 }
+
+console.log(countAnimals());
 
 module.exports = countAnimals;
--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -4,27 +4,17 @@ function isManager(id) {
   return data.employees.some((currentValue) => currentValue.managers.includes(id));
 }
 
-const test = (subordinatesArray) => {
-  if (subordinatesArray.length === 0) {
-    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
-  }
-};
-
 function getRelatedEmployees(managerId) {
   const manager = data.employees.filter(
     (currentValue) => currentValue.managers.includes(managerId),
   );
-  const subordinates = manager.map(
+  if (manager.length === 0) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const subordinatesNames = manager.map(
     (currentValue) => `${currentValue.firstName} ${currentValue.lastName}`,
   );
-  try {
-    test(subordinates);
-    return subordinates;
-  } catch (error) {
-    throw error.message;
-  }
+  return subordinatesNames;
 }
-
-console.log(getRelatedEmployees('4b40a139-d4dc-4f09-822d-ec25e819a5ad'));
 
 module.exports = { isManager, getRelatedEmployees };
