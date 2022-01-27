bcbb2a096498e0952cc044fc40451fe75b979e03
EduMLAlmeida <edualmeidawebdev@gmail.com>
2022-01-27 16:23:25 -0300

    atende ao requisito 6

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,42 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  let quantAdult = 0;
+  let quantChild = 0;
+  let quantSenior = 0;
+  entrants.forEach((currentValue) => {
+    if (currentValue.age < 18) {
+      quantChild += 1;
+    } else if (currentValue.age >= 50) {
+      quantSenior += 1;
+    } else {
+      quantAdult += 1;
+    }
+  });
+  const quantAll = { adult: quantAdult, child: quantChild, senior: quantSenior };
+  return quantAll;
+}
+
+function calculatePrice(visitantsObject) {
+  const visitantsArray = Object.entries(visitantsObject);
+  let price = 0;
+  visitantsArray.forEach((currentValue) => {
+    price += data.prices[currentValue[0]] * currentValue[1];
+  });
+  return price;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (typeof (entrants) === 'object') {
+    const emptyObjectTest = Object.entries(entrants);
+    if (emptyObjectTest.length === 0) {
+      return 0;
+    }
+  }
+  if (entrants === undefined) {
+    return 0;
+  }
+  return calculatePrice(countEntrants(entrants));
 }
 
 module.exports = { calculateEntry, countEntrants };
