8127560acb0f012bd358fec5401e270d5ab488d6
EduMLAlmeida <edualmeidawebdev@gmail.com>
2022-01-31 14:55:37 -0300

    Atende ao requisito 9

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,25 @@
 const data = require('../data/zoo_data');
 
+const findAnimalID = (employeeID) => data.employees.find(
+  (element) => element.id === employeeID,
+).responsibleFor[0];
+
+const findResidentAnimalsArray = (animalID) => data.species.find(
+  (element) => element.id === animalID,
+).residents;
+
+function findOlderAnimal(animalObjectsArray) {
+  let olderAnimal = animalObjectsArray[0];
+  animalObjectsArray.forEach((element) => {
+    if (element.age > olderAnimal.age) {
+      olderAnimal = element;
+    }
+  });
+  return Object.values(olderAnimal);
+}
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  return findOlderAnimal(findResidentAnimalsArray(findAnimalID(id)));
 }
 
 module.exports = getOldestFromFirstSpecies;
